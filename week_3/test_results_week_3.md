2e0b49e229ddb787431d7131de8e97e9741ceeb5
Sri Bala <srirocks2020@sri-mac.local>
2025-01-24 19:54:52 -0800

    pixel counts

--- week_3/test_results_week_3.md
+++ week_3/test_results_week_3.md
@@ -8,10 +8,22 @@
 ### Ranking of Colors by Distinct Users
 
 - **Top **
-  1. Black: 152 users
-  2. Red: 120 users
-  3. Blue: 98 users
-     etc.
+1. black : 412124 users
+2. orangered : 299003 users
+3. white : 270212 users
+4. #2450A4 : 244148 users
+5. #FFD635 : 201025 users
+6. #51E9F4 : 117115 users
+7. #FF99AA : 116719 users
+8. #7EED56 : 102042 users
+9. #00A368 : 95611 users
+10. #811E9F : 77678 users
+11. #FFA800 : 77006 users
+12. #3690EA : 69604 users
+13. #D4D7D9 : 38707 users
+14. #898D90 : 37128 users
+15. #9C6926 : 30551 users
+16. #B44AC0 : 29539 users
 
 ### Average Session Length
 
@@ -27,7 +39,7 @@
 
 ### Count of First-Time Users
 
-- **Output:** 350 users
+- **Output:** 1062699
 
 ### Runtime
 
--- week_3/w3_analysis.py
+++ week_3/w3_analysis.py
@@ -2,27 +2,32 @@ from time import perf_counter_ns
 import sys
 from datetime import datetime
 import duckdb
+import webcolors
 
 
-def rank_colors(start, end):
+def rank_colors(conn, start, end):
     """
     rank colors by # of distinct users that placed those colors
     """
-    start_counter = perf_counter_ns()
-    connection = duckdb.connect()
-    results = f"""
-        SELECT pixel_color
+
+    get_ranking = f"""
+        SELECT pixel_color, COUNT(DISTINCT user_id) as distinct_users
         FROM './2022_pyarrow.parquet'
         WHERE timestamp >= '{start}' 
           AND timestamp < '{end}'
-        GROUP BY user_id
-        ORDER BY COUNT(*) DESC
-        LIMIT 1
+        GROUP BY pixel_color
+        ORDER BY distinct_users DESC
     """
+    rankings = conn.execute(get_ranking).fetchall()
+    for i in range(len(rankings)):
+        try:
+            color = webcolors.hex_to_name(rankings[i][0])
+        except ValueError:
+            color = rankings[i][0]                         
 
-    end_counter = perf_counter_ns()
+        print (i+1,"-", color,":",rankings[i][1], "users")
 
-    return ranking
+    
 
 
 def avg_session_length(start, end):
@@ -38,12 +43,30 @@ def pixel_placements(start, end):
     50th, 75th, 90th, and 99th percentiles of num pixels
     """
 
+    
 
-def count_first_time_users(start, end):
+
+def count_first_time_users(conn, start, end):
     """
     how many users placed their first pixel ever within the specified timeframe
+    1. get first placement time for each user
+    2. filter users where first time falls between start and end.
     """
 
+    first_pixel = f"""
+        SELECT COUNT(*)
+        FROM (
+            SELECT user_id, MIN(timestamp) AS first_placement
+            FROM './2022_pyarrow.parquet'
+            GROUP BY user_id
+        )
+        WHERE first_placement >= '{start}'
+            AND first_placement < '{end}'
+    """
+
+    return conn.execute(first_pixel).fetchone()[0]
+
+    
 
 if __name__ == "__main__":
 
@@ -58,7 +81,11 @@ if __name__ == "__main__":
         print("End hour must be after start hour")
         sys.exit(1)
     else:
+        start_counter = perf_counter_ns()
         start_dt = datetime.strptime(start, "%Y-%m-%d %H")
         end_dt = datetime.strptime(end, "%Y-%m-%d %H")
-
-        ranked_colors = rank_colors(start_dt, end_dt)
+        conn = duckdb.connect()
+        print("ranking:")
+        ranked_colors = rank_colors(conn, start_dt, end_dt)
+        print("\n # first time users: ", count_first_time_users(conn, start_dt, end_dt),"\n")
+        end_counter = perf_counter_ns()
